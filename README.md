3298cedb2b441bbe845760bc70482128560ce8af
Jon Jagger <jon@jaggersoft.com>
2018-02-09 08:53:13 +0000

    removed version tag from image name; do version check in script and not in Dockerfile

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# java 1.8
+# Java
 
-![Build Status](https://travis-ci.org/cyber-dojo-languages/java-1.8.svg?branch=master)
+![Build Status](https://travis-ci.org/cyber-dojo-languages/java.svg?branch=master)
+
+Version=1.8
---
+++ check_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
+readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/java sh -c 'javac --version 2>&1')
+
+if echo ${ACTUAL} | grep -q ${EXPECTED}; then
+  echo "VERSION CONFIRMED as ${EXPECTED}"
+else
+  echo "VERSION EXPECTED: ${EXPECTED}"
+  echo "VERSION   ACTUAL: ${ACTUAL}"
+  exit 1
+fi
