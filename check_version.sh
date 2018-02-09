8bc74d55cc65f6f992cc8e6c88687592e9da126a
Jon Jagger <jon@jaggersoft.com>
2018-02-09 09:21:14 +0000

    fix broken version-check script

--- check_version.sh
+++ check_version.sh
@@ -2,7 +2,7 @@
 set -e
 
 readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
-readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/java sh -c 'javac --version 2>&1')
+readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/java sh -c 'javac -version 2>&1')
 
 if echo ${ACTUAL} | grep -q ${EXPECTED}; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
