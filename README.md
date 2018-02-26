bab289dd6cec9514ce6497011cd80f730bf497d3
Jon Jagger <jon@jaggersoft.com>
2018-02-26 15:57:31 +0000

    added hyperlink to readme version number

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ![Build Status](https://travis-ci.org/cyber-dojo-languages/java.svg?branch=master)
 
-Version=1.8
+[Version=1.8](https://github.com/cyber-dojo-languages/java/blob/master/check_version.sh)
--- check_version.sh
+++ check_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
+readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
 readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/java sh -c 'javac -version 2>&1')
 
 if echo ${ACTUAL} | grep -q ${EXPECTED}; then
