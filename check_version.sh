bf740c8c15a97095df8155d2bd26eb8f17a1aa84
Jon Jagger <jon@jaggersoft.com>
2019-06-07 14:32:40 +0100

    switch back to usual image-builder script

--- check_version.sh
+++ check_version.sh
@@ -5,7 +5,7 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly EXPECTED=$(cat ${MY_DIR}/README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
 readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/java sh -c 'javac -version 2>&1')
 
-if echo ${ACTUAL} | grep -q ${EXPECTED}; then
+if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
 else
   echo "VERSION EXPECTED: ${EXPECTED}"
