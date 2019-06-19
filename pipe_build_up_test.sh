154ca3cc30d7c9fb427708f218ff053acad9fd1c
Jon Jagger <jon@jaggersoft.com>
2019-06-19 20:36:45 +0100

    add my-dir argument to build script

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -3,4 +3,4 @@ set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-${MY_DIR}/../image_builder/build_test_push_notify.sh
+${MY_DIR}/../image_builder/build_test_push_notify.sh "${MY_DIR}"
